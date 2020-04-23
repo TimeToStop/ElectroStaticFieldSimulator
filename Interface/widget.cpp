d32f794f1fa81a27ad88aac0dd9533ca91282aae
TimeToStop <evgrat123@mail.ru>
2020-04-23 14:02:05 +0300

    Refactor UI

--- Interface/widget.cpp
+++ Interface/widget.cpp
@@ -24,16 +24,26 @@ Widget::Widget(QWidget *parent):
 
     // Engine Widget
     Engine* engine = new Engine();
-
     // Main UI Buttons
     QWidget* widget = new QWidget();
 
+    widget->setMaximumSize(400, 550);
+
+    main->addWidget(engine);
+    main->addWidget(widget);
+
+    // Layout
     QVBoxLayout* settings = new QVBoxLayout();
+    widget->setLayout(settings);
 
     // Working with charges
     QGroupBox* charges_box = new QGroupBox("Charges");
     QVBoxLayout* charges_buttons = new QVBoxLayout();
 
+    charges_box->setLayout(charges_buttons);
+    settings->addWidget(charges_box);
+
+    // Buttons to work with charges
     QPushButton* add_charge = new QPushButton("Add charge");
     QPushButton* ignore_charges = new QPushButton("Ignore charges");
     QPushButton* edit_charge = new QPushButton("Edit charge");
@@ -43,142 +53,122 @@ Widget::Widget(QWidget *parent):
     charges_buttons->addWidget(ignore_charges);
     charges_buttons->addWidget(edit_charge);
     charges_buttons->addWidget(rm_charge);
-
-    charges_box->setLayout(charges_buttons);
-    settings->addWidget(charges_box);
     // End of working with charges
 
     // Tab: E Y A W
     QTabWidget* tab = new QTabWidget();
+    settings->addWidget(tab);
+
+    QWidget* tension = new QWidget();
+    QWidget* potential = new QWidget();
+    QWidget* work = new QWidget();
+    QWidget* energy = new QWidget();
+
+    tab->addTab(tension, "Tension");
+    tab->addTab(potential, "Potential");
+    tab->addTab(work, "Work");
+    tab->addTab(energy, "Energy");
 
     // E: tension
-    QWidget* tension = new QWidget();
-
     QHBoxLayout* tension_layout = new QHBoxLayout();
+    tension->setLayout(tension_layout);
 
     // Postion of tension to calculate
     QVBoxLayout* tension_pos = new QVBoxLayout();
+    tension_layout->addLayout(tension_pos);
     QCheckBox* tension_check = new QCheckBox("Use cursor position");
-
     tension_pos->addWidget(tension_check);
 
     QGroupBox* tension_pos_box = new QGroupBox("Position");
     QVBoxLayout* tension_position = new QVBoxLayout();
+    tension_pos_box->setLayout(tension_position);
+    tension_pos->addWidget(tension_pos_box);
 
     ValueRepresent* tension_pos_x = new ValueRepresent("X", "м");
     ValueRepresent* tension_pos_y = new ValueRepresent("Y", "м");
-
     tension_position->addWidget(tension_pos_x);
     tension_position->addWidget(tension_pos_y);
-
-    tension_pos_box->setLayout(tension_position);
-    tension_pos->addWidget(tension_pos_box);
     // End of Working with position
 
     // Calculated Result of tension
     QGroupBox* tension_box = new QGroupBox("Tension");
+    tension_layout->addWidget(tension_box);
     QVBoxLayout* tension_res_layout = new QVBoxLayout();
+    tension_box->setLayout(tension_res_layout);
 
     ValueRepresent* tension_val = new ValueRepresent("E", "Н/м");
     ValueRepresent* tension_x = new ValueRepresent("Ex", "Н/м");
     ValueRepresent* tension_y = new ValueRepresent("Ey", "Н/м");
-
     tension_res_layout->addWidget(tension_val);
     tension_res_layout->addWidget(tension_x);
     tension_res_layout->addWidget(tension_y);
-
-    tension_box->setLayout(tension_res_layout);
-
-    tension_layout->addLayout(tension_pos);
-    tension_layout->addWidget(tension_box);
-    tension->setLayout(tension_layout);
     // End of working with calculated tension
-    tab->addTab(tension, "Tension");
     // End of tension
 
     // Y: Potential
-    QWidget* potential = new QWidget();
-
     QHBoxLayout* potential_layout = new QHBoxLayout();
+    potential->setLayout(potential_layout);
 
     // Postion of Potential to calculate
     QVBoxLayout* potential_pos = new QVBoxLayout();
-    QCheckBox* potential_check = new QCheckBox("Use cursor position");
+    potential_layout->addLayout(potential_pos);
 
+    QCheckBox* potential_check = new QCheckBox("Use cursor position");
     potential_pos->addWidget(potential_check);
 
     QGroupBox* potential_pos_box = new QGroupBox("Position");
+    potential_pos->addWidget(potential_pos_box);
     QVBoxLayout* potential_position = new QVBoxLayout();
+    potential_pos_box->setLayout(potential_position);
 
     ValueRepresent* potential_pos_x = new ValueRepresent("X", "м");
     ValueRepresent* potential_pos_y = new ValueRepresent("Y", "м");
-
     potential_position->addWidget(potential_pos_x);
     potential_position->addWidget(potential_pos_y);
-
-    potential_pos_box->setLayout(potential_position);
-    potential_pos->addWidget(potential_pos_box);
-    potential_layout->addLayout(potential_pos);
     // End of Working with position
 
     QVBoxLayout* potential_value = new QVBoxLayout();
+    potential_layout->addLayout(potential_value);
 
     ValueRepresent* potential_val = new ValueRepresent("Y", "В");
     QSpacerItem* spacer1 = new QSpacerItem(1, 100);
     potential_value->addWidget(potential_val);
     potential_value->addSpacerItem(spacer1);
-
-    potential_layout->addLayout(potential_value);
-    potential->setLayout(potential_layout);
-
-    tab->addTab(potential, "Potential");
     // End of potential
 
-    settings->addWidget(tab);
-    // End tab
-
     QGroupBox* additional = new QGroupBox("Additional");
+    settings->addWidget(additional);
     QVBoxLayout* additional_layout = new QVBoxLayout();
+    additional->setLayout(additional_layout);
 
     QHBoxLayout* camera = new QHBoxLayout();
+    additional_layout->addLayout(camera);
 
     QLabel* camera_at = new QLabel("Camera at:");
     QComboBox* camera_combo = new QComboBox();
-
     camera->addWidget(camera_at);
     camera->addWidget(camera_combo);
-    additional_layout->addLayout(camera);
 
     QHBoxLayout* scene = new QHBoxLayout();
+    additional_layout->addLayout(scene);
 
     QLabel* scene_at = new QLabel("Change scene:");
     QComboBox* scene_combo = new QComboBox();
-
-    camera->addWidget(scene_at);
-    camera->addWidget(scene_combo);
-    additional_layout->addLayout(scene);
+    scene->addWidget(scene_at);
+    scene->addWidget(scene_combo);
 
     QHBoxLayout* info = new QHBoxLayout();
+    additional_layout->addLayout(info);
 
     QLabel* scale_label = new QLabel("Scale:");
     QSlider* scale_slider = new QSlider(Qt::Horizontal);
+    info->addWidget(scale_label);
+    info->addWidget(scale_slider);
 
     QCheckBox* show_grid = new QCheckBox("Show grid");
     QCheckBox* show_tension = new QCheckBox("Show tension lines");
-
-    info->addWidget(scale_label);
-    info->addWidget(scale_slider);
     info->addWidget(show_grid);
     info->addWidget(show_tension);
-    additional_layout->addLayout(info);
-
-    additional->setLayout(additional_layout);
-    settings->addWidget(additional);
-
-    widget->setLayout(settings);
-    widget->setMaximumSize(400, 525);
-    main->addWidget(engine);
-    main->addWidget(widget);
 }
 
 Widget::~Widget()
