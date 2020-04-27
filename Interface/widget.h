4281ea3ec243964e19a9ddd21f66ee70fa43ffa8
slayer404 <doomer888@protonmail.com>
2020-04-27 14:59:10 +0300

    Added:   Functions which connect GUI with Engine

--- Interface/widget.h
+++ Interface/widget.h
@@ -27,6 +27,9 @@ class Widget : public QWidget
 
     ValueRepresent* m_potential_val;
 
+    bool m_use_cursor;
+
+
 public:
     explicit Widget(QWidget *parent = nullptr);
     virtual ~Widget();
@@ -40,7 +43,14 @@ public slots:
     void rmCharge();
 
     void showGrid(int);
-    void showTension(int);
+    void showElectrostaticField(int);
+
+    void calculateTension();
+    void calculateTension(float x, float y);
+    void calculateTensionByMouse(const QPoint&);
+    void calculatePotential();
+    void calculatePotential(float x, float y);
+    void calculatePotentialByMouse(const QPoint&);
 
     void scaleChanged(int);
 
